a8a9cc4eb0ab629fa7b43537bff23831199bac5c
naufil09 <snaufil09@gmail.com>
2021-06-05 14:28:29 +0530

    Remove unnecessary code.

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -7,15 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
   noteArray = [{}]; color = "#ecebed";
-  menu = [{ id: 1, name: "level1" }, { id: 2, name: "level2" }, { id: 3, name: "level3" },]
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  selectedIdx = 0;
-  selectItem(index: number): void {
-    this.selectedIdx = index;
   }
 
   onAddNote() {
